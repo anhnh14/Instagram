d7023eb0dbe8eae1a9b9a7bdbd634f13253fdc31
anhnh148 <sir.anhnguyena2@gmail.com>
2017-09-02 16:10:51 -0500

    - Add Interface

---
+++ DemoInstagram/APIsHelper/APIsInterface/IEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using DemoInstagram.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoInstagram.APIsHelper.APIsInterface
+{
+    public interface IEndpoint
+    {
+        Task<Profile> GetProfile();
+
+        Task<Picture> GetRecentImage();
+
+        Task<List<Profile>> SearchUser(string userName);
+
+        Task<Picture> GetImageRecentPublishByUser(string userId);
+
+        Task<bool> PostComment(string comment, string pictureId);
+
+        Task<List<Comment>> LoadComments(string imageId);
+
+        Task<bool> LikeImage(string pictureId);
+    }
+}
