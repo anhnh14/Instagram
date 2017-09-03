100f0de591af17f9fadc311adfafeb32f1aa655f
anhnh148 <sir.anhnguyena2@gmail.com>
2017-09-03 10:15:10 -0500

    - Update name Interface/IBusiness.cs

---
+++ DemoInstagram/APIsHelper/EndpointImpl.cs
@@ -0,0 +1,262 @@
+﻿using DemoInstagram.APIsHelper.APIsInterface;
+using DemoInstagram.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DemoInstagram.Business.Interface;
+
+namespace DemoInstagram.APIsHelper
+{
+    public class EndpointImpl : IEndpoint
+    {
+        private readonly IBusiness _instagramBussiness;
+
+        public EndpointImpl(IBusiness instagramBussiness)
+        {
+            this._instagramBussiness = instagramBussiness;
+        }
+
+        /// <summary>
+        /// Get profile about owner of the access_token. 
+        /// </summary>
+        /// <returns>Profile</returns>
+        public async Task<Profile> GetProfile()
+        {
+            string path = _instagramBussiness.GetPath(Configuaration.API_USER, "self/?access_token=");
+            //connect to API
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(path))
+                {
+                    Profile profile = new Profile();
+
+                    //check response from server
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //get profile of owner token
+                        var jsonString = response.Content.ReadAsStringAsync().Result;
+                        profile = _instagramBussiness.ProcessProfile(jsonString);
+                    }
+                    else
+                    {
+                        //Process json error from server
+                        throw new Exception(_instagramBussiness.ProcessError(response));
+                    }
+                    return profile;
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// Get the most recent media published by the owner of the access_token. 
+        /// </summary>
+        /// <returns>Picture</returns>
+        public async Task<Picture> GetRecentImage()
+        {
+            //Get path recent image
+            string path = _instagramBussiness.GetPath(Configuaration.API_USER, "self/media/recent?access_token=");
+
+            //Connect to API
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(path))
+                {
+                    List<string> list = new List<string>();
+                    Picture picture = new Picture();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //Get json from Server
+                        string jsonString = response.Content.ReadAsStringAsync().Result;
+
+                        //Process image from json
+                        picture = _instagramBussiness.ProcessPicture(jsonString);
+                    }
+                    else
+                    {
+                        throw new Exception(_instagramBussiness.ProcessError(response));
+                    }
+                    return picture;
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// Get a list of users matching the query. 
+        /// </summary>
+        /// <param name="userName">input name of user</param>
+        /// <returns>List of user</returns>
+        public async Task<List<Profile>> SearchUser(string userName)
+        {
+            //Get path list user 
+            string path = _instagramBussiness.GetPath(Configuaration.API_USER, "search?q=" + userName + "&&access_token=");
+
+            //Connect to API 
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(path))
+                {
+                    List<Profile> listProfile = new List<Profile>();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //Get json string from response
+                        var jsonString = response.Content.ReadAsStringAsync().Result;
+
+                        //process list profile from json
+                        listProfile = _instagramBussiness.ProcessListProfile(jsonString);
+                    }
+                    else
+                    {
+                        throw new Exception(_instagramBussiness.ProcessError(response));
+                    }
+                    return listProfile;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Get the most recent media published by user from search list
+        /// </summary>
+        /// <param name="userId">user id from instagram</param>
+        /// <returns>picture</returns>
+        public async Task<Picture> GetImageRecentPublishByUser(string userId)
+        {
+
+            //Get path image recent published by user
+            string path = _instagramBussiness.GetPath(Configuaration.API_USER, userId + "/media/recent/?access_token=");
+
+            //Connect to API
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(path))
+                {
+                    List<string> list = new List<string>();
+                    Picture picture = new Picture();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //Get json from reponse
+                        string jsonString = response.Content.ReadAsStringAsync().Result;
+
+                        //Process image from json
+                        picture = _instagramBussiness.ProcessPicture(jsonString);
+                    }
+                    else
+                    {
+                        throw new Exception(_instagramBussiness.ProcessError(response));
+                    }
+                    return picture;
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Post comment to image
+        /// </summary>
+        /// <param name="comment">content of comment</param>
+        /// <param name="pictureId">id of image</param>
+        /// <returns>true if post comment success else return false</returns>
+        public async Task<bool> PostComment(string comment, string pictureId)
+        {
+            //Connect to API
+            using (var client = new HttpClient())
+            {
+                //Prepare comment from user input
+                var requestContent = new FormUrlEncodedContent(new[] {
+                    new KeyValuePair<string, string>(Configuaration.ACCESS_TOKEN,Global.TOKEN),
+                    new KeyValuePair<string, string>(Configuaration.KEY_API_TEXT, comment)
+                });
+                HttpResponseMessage response = new HttpResponseMessage();
+
+                //post comment
+                response = await client.PostAsync(Configuaration.API_MEDIA + pictureId + "/comments", requestContent);
+
+                //check status
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Load comment from image
+        /// </summary>
+        /// <param name="imageId">id of image</param>
+        /// <returns>List comment</returns>
+        public async Task<List<Comment>> LoadComments(string imageId)
+        {
+            //Get path list comment
+            string path = _instagramBussiness.GetPath(Configuaration.API_MEDIA, imageId + "/comments?access_token=");
+
+            //Connect to API
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(path))
+                {
+                    List<Comment> listComment = new List<Comment>();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //Get json from response
+                        var jsonString = response.Content.ReadAsStringAsync().Result;
+
+                        //Process list comment from json
+                        listComment = _instagramBussiness.LoadComment(jsonString);
+                    }
+                    else
+                    {
+                        //Process error json from server
+                        throw new Exception(_instagramBussiness.ProcessError(response));
+                    }
+                    return listComment;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Like image
+        /// </summary>
+        /// <param name="pictureId">id of image</param>
+        public async Task<bool> LikeImage(string pictureId)
+        {
+            //Connect to API
+            using (var client = new HttpClient())
+            {
+                //Prepare data to like
+                //Add token to url
+                var requestContent = new FormUrlEncodedContent(new[] {
+                    new KeyValuePair<string, string>(Configuaration.ACCESS_TOKEN,Global.TOKEN),
+                });
+
+                //Send like request
+                HttpResponseMessage response = await client.PostAsync(Configuaration.API_MEDIA + pictureId + "/likes", requestContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+    }
+}
---
+++ DemoInstagram/Business/Interface/IBusiness.cs
@@ -0,0 +1,21 @@
+﻿using DemoInstagram.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace DemoInstagram.Business.Interface
+{
+    public interface IBusiness
+    {
+        string GetPath(string typeAPI, string parameter);
+
+        Profile ProcessProfile(string json);
+
+        Picture ProcessPicture(string json);
+
+        List<Profile> ProcessListProfile(string json);
+
+        List<Comment> LoadComment(string json);
+
+        string ProcessError(HttpResponseMessage response);
+    }
+}
